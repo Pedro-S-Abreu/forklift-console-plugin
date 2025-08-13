3c027e3dee96f5ae4e94f9ee99093961193f52b7
Pedro Abreu <pabreu@redhat.com>
2025-08-13 07:37:31 -0400

    debug 3
    Signed-off-by: Pedro Abreu <pabreu@redhat.com>

--- testing/playwright/page-objects/PlansListPage.ts
+++ testing/playwright/page-objects/PlansListPage.ts
@@ -25,9 +25,35 @@ export class PlansListPage {
   }
 
   async navigateFromMainMenu() {
-    await disableGuidedTour(this.page);
-    await this.page.goto('/');
+    // eslint-disable-next-line no-console
+    console.log(`Navigating to /`);
+    try {
+      const response = await this.page.goto('/');
+      // eslint-disable-next-line no-console
+      console.log(`Navigation to / completed.`);
+      if (response) {
+        // eslint-disable-next-line no-console
+        console.log(`Response status: ${response.status()}`);
+        const isOk = response.ok();
+        // eslint-disable-next-line no-console
+        console.log(`Response ok: ${isOk}`);
+        if (!isOk) {
+          // eslint-disable-next-line no-console
+          console.log(`Response text: ${await response.text()}`);
+        }
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('No response from page.goto("/")');
+      }
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(`Error navigating to /: ${e.message}`);
+      // eslint-disable-next-line no-console
+      console.error(e.stack);
+      throw e;
+    }
 
+    await disableGuidedTour(this.page);
     await waitForLoader(this.page);
     await this.page.getByTestId('migration-nav-item').click();
     await this.page.getByTestId('plans-nav-item').click();
